f8e1c58d094faf722405110d665e941939c828ce
softara1 <almoatn2@gmail.com>
2026-08-09 17:26:40 +0400

    Update soft.js

--- Softara/js/soft.js
+++ Softara/js/soft.js
@@ -2,8 +2,8 @@
 // كود حماية حقوق التصميم 
 // ==========================================
 (function() {
-    const myUrl = "https://www.facebook.com/Flying1free"; 
-    const myName = "تصميم و برمجة :- الطائر الحر";
+    const myUrl = "https://www.facebook.com/awad.alsudan"; 
+    const myName = "تصميم و برمجة :- عوض السوداني";
 
     function checkDesignerRights() {
         var linkEl = document.getElementById('designer-copy-link');
